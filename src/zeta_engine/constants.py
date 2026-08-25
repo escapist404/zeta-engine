6de7302229bb050e17a0e4e709f9e7d385edd196
escapist404 <123872320+escapist404@users.noreply.github.com>
2026-08-25 22:05:44 +0800

    change constants (domains)

--- src/zeta_engine/constants.py
+++ src/zeta_engine/constants.py
@@ -1,6 +1,24 @@
 SEED_URLS = (
     "http://pd.ruc.edu.cn/",
     "http://sph.ruc.edu.cn/",
+    "https://clr.ruc.edu.cn/zwwz/index.htm",
+    "http://dis.ruc.edu.cn/",
+    "http://dsdj.ruc.edu.cn/",
+    "http://scsce.ruc.edu.cn/",
+    "http://isbd.ruc.edu.cn/",
+    "http://sis.ruc.edu.cn/",
+    "http://info.ruc.edu.cn/",
+    "http://www.phys.ruc.edu.cn/",
+    "http://psy.ruc.edu.cn/",
+    "http://guoxue.ruc.edu.cn/",
+    "https://envi.ruc.edu.cn/",
+    "http://ai.ruc.edu.cn/", 
+    "https://gsai.ruc.edu.cn"
+)
+
+ALLOWED_DOMAINS = (
+    "http://pd.ruc.edu.cn/",
+    "http://sph.ruc.edu.cn/",
     "https://clr.ruc.edu.cn/",
     "http://dis.ruc.edu.cn/",
     "http://dsdj.ruc.edu.cn/",
